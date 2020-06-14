ad9006a52d9e6a43c5b9be36e56539a7abe5d5a7
Anthony N <antnat96@gmail.com>
2020-06-14 14:53:23 -0500

    implementation files

---
+++ LRUBufferBlock.h
@@ -0,0 +1,23 @@
+/*
+ * File:   LRUBufferBlock.h
+ * Author: Anthony Natale
+ * Created on June 14, 2020
+ */
+
+#ifndef LRUBUFFERBLOCK_H
+#define	LRUBUFFERBLOCK_H
+
+#include <string>
+#include "BufferBlockADT.h"
+using namespace std;
+
+// ADT for buffer pools using the message-passing style
+class LRUBufferBlock : public BufferblockADT {
+private:
+
+public:
+
+};
+
+#endif	/* LRUBUFFERBLOCK_H */
+
---
+++ LRUBufferPool.h
@@ -0,0 +1,23 @@
+/*
+ * File:   LRUBufferPool.h
+ * Author: Anthony Natale
+ * Created on June 14, 2020
+ */
+
+#ifndef LRUBUFFERPOOL_H
+#define	LRUBUFFERPOOL_H
+
+#include <string>
+#include "BufferPoolADT.h"
+using namespace std;
+
+// ADT for buffer pools using the message-passing style
+class LRUBufferPool : public BufferPoolADT {
+private:
+
+public:
+
+};
+
+#endif	/* LRUBUFFERPOOL_H */
+
